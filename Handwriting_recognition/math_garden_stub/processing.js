1d72f14f717b491c4fe1ffd6711d52af156aba0c
foyezz <38753216+foyezz@users.noreply.github.com>
2023-05-09 17:42:34 +0300

    calculating center of mass and shifting the image

--- Handwriting_recognition/math_garden_stub/processing.js
+++ Handwriting_recognition/math_garden_stub/processing.js
@@ -25,6 +25,59 @@ function predictImage() {
     image = image.roi(rect);
 
 
+    var height = image.rows;
+    var width = image.cols;
+
+    if (height > width) {
+        height = 20;
+        const scaleFactor = image.rows / height;
+        width = Math.round(image.cols / scaleFactor);
+    }   else {
+        width = 20;
+        const scaleFactor = image.cols / width;
+        height = Math.round(image.rows / scaleFactor);
+    }
+
+    let newSize = new cv.Size(width, height);
+    cv.resize(image, image, newSize, 0, 0, cv.INTER_AREA)
+
+    const LEFT = Math.ceil(4 + (20 - width) / 2);
+    const RIGHT = Math.floor(4 + (20 - width) / 2);
+    const TOP = Math.ceil(4 + (20 - height) / 2);
+    const BOTTOM = Math.floor(4 + (20 - height) / 2);
+
+    const BLACK = new cv.Scalar(0, 0, 0, 0);
+    cv.copyMakeBorder(image, image, TOP, BOTTOM, LEFT, RIGHT, cv.BORDER_CONSTANT, BLACK);
+
+
+    // centre of mass
+    cv.findContours(image, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
+    cnt = contours.get(0);
+    const Moments = cv.moments(cnt, false);
+
+    const cx = Moments.m10 / Moments.m00;
+    const cy = Moments.m01 / Moments.m00;
+
+    const X_SHIFT = Math.round(image.cols/2.0 - cx);
+    const Y_SHIFT = Math.round(image.rows/2.0 - cy);
+
+    newSize = new cv.Size(image.cols, image.rows);
+    const M = cv.matFromArray(2, 3, cv.CV_64FC1, [1, 0, X_SHIFT, 0, 1, Y_SHIFT]);
+    cv.warpAffine(image, image, M, newSize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, BLACK);
+
+
+
+    let pixelValues = image.data;
+
+    pixelValues = Float32Array.from(pixelValues);
+
+    pixelValues = pixelValues.map(function (item) {
+        return item/255.0;
+    });
+
+    console.log(`Scale Array: ${pixelValues}`);
+
+
     //test code 
 
     const outputCanavas = document.createElement('canvas');
@@ -37,5 +90,6 @@ function predictImage() {
     contours.delete(),
     cnt.delete();
     hierarchy.delete();
+    M.delete();
 
 }
